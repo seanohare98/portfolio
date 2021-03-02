b8e85d6ea34c53878a215327ec647cb2ff03408d
seanohare98 <seanohare@link.cuhk.edu.hk>
2021-03-02 16:17:40 +0800

    Updates

--- precache-manifest.483e02135afadf004c34289e232a3b3f.js
+++ precache-manifest.483e02135afadf004c34289e232a3b3f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c444a20702fec84434dd",
+    "revision": "a3e4602301f71a59923a",
     "url": "/static/css/main.0e2d5b41.chunk.css"
   },
   {
-    "revision": "c444a20702fec84434dd",
-    "url": "/static/js/main.c444a207.chunk.js"
+    "revision": "a3e4602301f71a59923a",
+    "url": "/static/js/main.a3e46023.chunk.js"
   },
   {
     "revision": "63a8f196648076aa1c3d",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "82b41bca5d7aeb3d645b78f0511788c1",
+    "revision": "f5330de74983779de1b367460195a7e8",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b949524e16a9c5ec1b5b2383d9abc11.js"
+  "/precache-manifest.483e02135afadf004c34289e232a3b3f.js"
 );
 
 workbox.clientsClaim();
