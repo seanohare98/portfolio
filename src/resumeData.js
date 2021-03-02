63647e49d7e248ce45ac9cbf67ed1f06e62891f7
seanohare98 <seanohare@link.cuhk.edu.hk>
2021-03-02 16:51:55 +0800

    cleanup

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,9 +1,9 @@
 let resumeData = {
     "imagebaseurl": "https://seanohare98.github.io/",
     "name": "Sean O'Hare",
-    "role": "", //US Citizen born in Tokyo. Having grown up 1/2 in Michigan and 1/2 in Shanghai, China has strongly shaped the way I operate on professional and personal levels. Transferred to CUHK from Michigan State for non-academic reasons, but currently interviewing for 2020 internships in the US. Always trying to write cleaner and more optimized code. Although I spend most of my on-screen time in the office as a part-time SWE Intern, I can promise significant contributions to some exciting passions projects throughout this year! Also an avid track and field fan/competitor, with a long jump PR of 6.82m or 22ft5inches. I've competed in NCAA DI meets with MSU Track and Field and meets in Hong Kong with CUHK Track and Field.
+    "role": "",
     "linkedinId": "sean-ohare",
-    "roleDescription": "Computer Science student @ The Chinese University of Hong Kong. Graduating May 2021.",
+    "roleDescription": "Computer Science student @ The Chinese University of Hong Kong.",
     "socialLinks": [
         {
             "name": "linkedin",
@@ -17,97 +17,35 @@ let resumeData = {
         },
         {
             "name": "resume",
-            "url" : "https://drive.google.com/file/d/1Vcsf3a_iSpFv0WCZqLMdSRCcWBM8In7R/view?usp=sharing",
-            "className" : "fa fa-file"
+            "url": "https://drive.google.com/file/d/1Vcsf3a_iSpFv0WCZqLMdSRCcWBM8In7R/view?usp=sharing",
+            "className": "fa fa-file"
         }
     ],
-    "aboutme": "Hi there", //"US Citizen born in Tokyo. Having grown up half in Michigan and half in Shanghai has very much shaped the way I operate on personal and professional levels. Transferred to Hong Kong from Michigan State for non-academic reasons. Also an avid track and field fan/competitor, with a long jump PR of 6.82 meters or 22.5 feet. I've competed in NCAA DI meets with MSU Track and Field and meets in Hong Kong with CUHK Track and Field.",
-    "address": "Based out of Hong Kong/Michigan",
+    "aboutme": "I'm Sean & I like to code.",
     "website": "seanohare98@gmail.com",
-    "education": [
-        {
-            "UniversityName": "The Chinese University of Hong Kong",
-            "specialization": "Bachelor of Science in Computer Science",
-            "MonthOfPassing": "May",
-            "YearOfPassing": "2021",
-            "Achievements": "Honors at Entrance, University Admission Scholarship, Faculty of Engineering Admission Scholarship, Track and Field Student-Athlete"
-        },
-        {
-            "UniversityName": "Michigan State University",
-            "specialization": "Bachelor of Science in Computer Science",
-            "YearOfPassing": "Transferred",
-            "Achievements": "Charles Drew Science Scholar, Design for America: MSU, Track and Field Student-Athlete"
-        }
-    ],
-    "work": [
-        {
-            "CompanyName": "Invest Bots Limited",
-            "specialization": "Software Engineering Intern",
-            "YearOfLeaving": "May 2019 - Present",
-            "Achievements1": "- Manage the company's primary stock data API and associated codebase/OpenAPI documentation",
-            "Achievements2": "- Reduced several endpoint-response times by up to 3x through implementation of Redis caching and improved SQL queries/server-side logic",
-            "Achievements3": "- Implemented a CI/CD pipeline for 4 company GitLab repos using a docker-in-docker approach, with automated testing (Jest, SuperTest), staging, and deployment to Kubernetes",
-            "Achievements4": "- Built React components for displaying data-tables and graphs for use by clients and internal BAs",
-
-        },
-        {
-            "CompanyName": "MSU Information Technology",
-            "specialization": "Information Technology Intern",
-            "YearOfLeaving": "May 2018 - August 2018",
-            "Achievements1": "- Built and deployed a website (www.msumigration.info) to assist with the oversight of a campus-wide migration project by posting weekly schedule and progress notifications",
-            "Achievements2": "- Wrote several PowerShell scripts to handle Active Directory user management functions",
-            "Achievements3": "- Managed weekly Excel spreadsheets containing migration wave plans and positions for 25 other employees",
-            "Achievements4": "- Provided technical support and documentation on an ad-hoc basis",
-        },
-        {
-            "CompanyName": "Time Out Shanghai",
-            "specialization": "Editorial Intern",
-            "YearOfLeaving": "June 2017 - August 2017",
-            "Achievements1": "- Edited dozens of pages on the Time Out Shanghai website, using HTML/CSS through a proprietary CMS, to be in accordance with the brand's style guide",
-            "Achievements2": "- Wrote and published 5 blog posts on upcoming events",
-            "Achievements3": "- Produced digital content using Adobe Photoshop and Final Cut Pro",
-            "Achievements4": "- Gathered information for news stories and organized events",
-        }
-        ],
+    "education": [],
+    "work": [],
     "portfolio": [
         {
             "name": "DormLink",
             "description":
-                "",
+                "Roommate-matching social networking app",
             "url": "https://github.com/seanohare98/dormlink",
             "imgurl": "images/portfolio/dormlink.png"
         },
         {
-            "name": "Track and Field Ranking System",
-            "description":
-                "",
-            "url": "http://www.hktrackandfield.org",
-            "imgurl": "images/portfolio/hktf.jpg"
-        },
-        {
             "name": "AR Beer Pong",
             "description":
-                "",
+                "Have fun social distancing!",
             "url": "https://github.com/seanohare98/ar-beer-pong",
-            "imgurl": "images/portfolio/bp.png"
+            "imgurl": "images/portfolio/bp.jpg"
         },
         {
-            "name": "File Transfer Protocol",
-            "description": "",
-            "url": "https://github.com/seanohare98/multithreaded-ftp-server",
-            "imgurl": "images/portfolio/ftp.jpg"
-        },
-        {
-            "name": "AVL Tree",
-            "description": "",
-            "url": "https://github.com/seanohare98/avl-tree",
-            "imgurl": "images/portfolio/avl.jpg"
-        },
-        {
-            "name": "Unix Shell",
-            "description": "",
-            "url": "https://github.com/seanohare98/unix-shell",
-            "imgurl": "images/portfolio/shell.png"
+            "name": "Track and Field Ranking System",
+            "description":
+                "View the latest (Hong Kong) track and field results",
+            "url": "http://www.hktrackandfield.org",
+            "imgurl": "images/portfolio/hktf.png"
         }
 
     ]
